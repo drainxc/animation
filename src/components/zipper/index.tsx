5450d8ec6ed1c1c97591278501de3ab816fc065e
eastcopper <ldh7228@gmail.com>
2022-03-28 19:56:02 +0900

    ⚡ :: (#5)[Zipper] Add Scroll event

---
+++ src/components/zipper/index.tsx
@@ -0,0 +1,39 @@
+import * as S from "./style";
+
+export default function Zipper() {
+
+  onscroll = function () {
+    let scroll: number =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+  };
+  return (
+    <>
+      <S.MainDiv>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+        <h1>asdf</h1>
+      </S.MainDiv>
+    </>
+  );
+}
