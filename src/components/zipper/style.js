9a83ae78fbd144de4a889196f88f4b74d165fe5d
eastcopper <ldh7228@gmail.com>
2022-03-28 19:38:06 +0900

    ⚡ :: (#4)[Zipper] Add font

---
+++ src/components/zipper/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+    font-family: "Pacifico", cursive;
+`;
