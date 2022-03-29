e27716c179cd779d3244c1815dcd265c09ee95cc
eastcopper <ldh7228@gmail.com>
2022-03-29 10:08:50 +0900

    ⚡ :: (#6)[Zipper] Add spans

--- src/components/zipper/index.tsx
+++ src/components/zipper/index.tsx
@@ -1,39 +1,28 @@
+import { useState } from "react";
 import * as S from "./style";
 
 export default function Zipper() {
+  const spanAmount: number = 20;
+  const spans: any = Array.from({ length: spanAmount }, () => {
+    return "";
+  })
+  const style = `${document.body.clientWidth / spanAmount}px`;
 
-  onscroll = function () {
-    let scroll: number =
-      document.documentElement.scrollTop || document.body.scrollTop;
-
-  };
   return (
     <>
       <S.MainDiv>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
-        <h1>asdf</h1>
+        {spans.map(() => (
+          <div
+            style={{
+              width: style,
+              height: "100%",
+              backgroundColor: "#D0D0D0",
+              display: "inline-flex",
+            }}
+          ></div>
+        ))}
       </S.MainDiv>
+      <S.Title>SCROLL</S.Title>
     </>
   );
 }
