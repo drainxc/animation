176d5647e75b5521c0b216f81238f8cfb19b8b0e
eastcopper <ldh7228@gmail.com>
2022-03-29 10:10:11 +0900

    ⚡ :: (#8)[Zipper] Design Zipper

--- src/components/zipper/style.js
+++ src/components/zipper/style.js
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 
 export const MainDiv = styled.div`
-    @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
-    font-family: "Pacifico", cursive;
+  position: fixed;
+  z-index: -1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #222222;
+  display: flex;
+`;
+
+export const Title = styled.p`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: clamp(3vw, 10vw, 5rem);
+  mix-blend-mode: difference;
+  color: #f2f2f2;
+  text-align: center;
+  line-height: 1.3;
 `;
