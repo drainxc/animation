aeaddf45468de807db7c89a9972e5a3a27d35b46
eastcopper <ldh7228@gmail.com>
2022-03-29 10:09:17 +0900

    ⚡ :: (#6)[Zipper] Add animation

--- src/components/zipper/index.tsx
+++ src/components/zipper/index.tsx
@@ -7,6 +7,29 @@ export default function Zipper() {
     return "";
   })
   const style = `${document.body.clientWidth / spanAmount}px`;
+  const [divTransfrom, setDivTransfrom] = useState("scale(1.05, 0)");
+
+  for (let i = 0; i < spanAmount; i++) {}
+
+  function showCurtainEffect(end: number, start: number, scroll: number) {
+    const gap = (end - start) / spanAmount;
+
+    spans.forEach((i: number) => {
+      const s = start + gap * i;
+      const e = s + gap * 10;
+      const ratio = (scroll - s) / (e - s);
+      const value = ratio > 1 ? 1.05 : ratio < 0 ? 0 : ratio;
+
+      setDivTransfrom(`scale(1.05, ${value})`);
+    });
+  }
+
+  onscroll = function () {
+    let scroll: number =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    showCurtainEffect(0, 3000, scroll);
+  };
 
   return (
     <>
@@ -14,6 +37,7 @@ export default function Zipper() {
         {spans.map(() => (
           <div
             style={{
+              transform: divTransfrom,
               width: style,
               height: "100%",
               backgroundColor: "#D0D0D0",
