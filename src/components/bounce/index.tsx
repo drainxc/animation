d8bdf2c2d0ffce59b3310c59c7f3c04da8c7237a
eastcopper <ldh7228@gmail.com>
2022-03-30 17:08:01 +0900

    ⚡ :: (#22)[Bounce] Add background, title

---
+++ src/components/bounce/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+export default function Bounce() {
+  return (
+    <>
+      <S.BackGround>
+      </S.BackGround>
+      <S.Title>Lee DongHyeon</S.Title>
+    </>
+  );
+}
