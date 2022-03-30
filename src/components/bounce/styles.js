51b8b73ea1a841cc37a66b25c63ab0e97695eec4
eastcopper <ldh7228@gmail.com>
2022-03-30 17:21:45 +0900

    ⚡ :: (#28)[Bounce] Add Up animation

--- src/components/bounce/styles.js
+++ src/components/bounce/styles.js
@@ -25,6 +25,7 @@ export const Move = styled.div`
   height: 75px;
   width: 75px;
   right: 0;
+  bottom: 0;
   animation: move 3s ease-in-out infinite;
 
   @keyframes move {
@@ -33,6 +34,36 @@ export const Move = styled.div`
     }
     100% {
       transform: translateX(-800px);
+    }
+  }
+`;
+
+export const Up = styled.div`
+  background-color: green;
+  position: absolute;
+  height: 75px;
+  width: 75px;
+  right: 0;
+  bottom: 0;
+
+  animation: up 3s cubic-bezier(0, 0, 0.4, 1) infinite;
+
+  @keyframes up {
+    0%,
+    20% {
+      transform: translateY(0rem);
+    }
+    35%,
+    50% {
+      transform: translateY(-15rem);
+    }
+    65%,
+    80% {
+      transform: translateY(-25rem);
+    }
+    90%,
+    100% {
+      transform: translateY(-27rem);
     }
   }
 `;
