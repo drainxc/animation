6723fa8f7a535fec30412060f3db05474e1b71ae
eastcopper <ldh7228@gmail.com>
2022-03-30 17:08:23 +0900

    ⚡ :: (#23)[Bounce] Design background, title

---
+++ src/components/bounce/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BackGround = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  height: 700px;
+  width: 800px;
+  background-color: grey;
+`;
+
+export const Title = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  font-family: "Pacifico", cursive;
+  font-size: 100px;
+`;
+
