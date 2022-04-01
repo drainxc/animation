7e8f5de27163ec0b2a442314dda15ca5de5a7630
eastcopper <ldh7228@gmail.com>
2022-04-01 11:03:20 +0900

    ⚡ :: (#38)[Bounce] Add BackGround

--- src/components/bounce/index.tsx
+++ src/components/bounce/index.tsx
@@ -3,16 +3,21 @@ import * as S from "./styles";
 export default function Bounce() {
   return (
     <>
-      <S.BackGround>
-        <S.Move>
-          <S.Up>
-            <S.Down>
-              <S.Ball></S.Ball>
-            </S.Down>
-          </S.Up>
-        </S.Move>
-      </S.BackGround>
-      <S.Title>Lee DongHyeon</S.Title>
+      <S.Background
+        width={document.body.clientWidth}
+        height={document.body.clientHeight}
+      >
+        <S.BallAnimation>
+          <S.Move>
+            <S.Up>
+              <S.Down>
+                <S.Ball></S.Ball>
+              </S.Down>
+            </S.Up>
+          </S.Move>
+        </S.BallAnimation>
+        <S.Title>Lee DongHyeon</S.Title>
+      </S.Background>
     </>
   );
 }
