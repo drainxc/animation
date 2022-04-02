f95d7a8b419c027b043776bd470f4a8082f8a6f6
eastcopper <ldh7228@gmail.com>
2022-04-03 01:15:32 +0900

    📑 :: (#47)[Error] Create Error

---
+++ src/components/error/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles";
+
+export default function Error() {
+  return (
+    <>
+      <S.ErrorDiv>
+        <S.Top>Lee DongHyeon</S.Top>
+        <S.Title>Lee DongHyeon</S.Title>
+        <S.Bottom>Lee DongHyeon</S.Bottom>
+      </S.ErrorDiv>
+    </>
+  );
+}
