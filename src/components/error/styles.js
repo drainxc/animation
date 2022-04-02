1426f9a653ea49863734019ff2af2351c58eb39c
eastcopper <ldh7228@gmail.com>
2022-04-03 01:23:16 +0900

    ⚡ :: (#49)[Error] Update Animation

--- src/components/error/styles.js
+++ src/components/error/styles.js
@@ -7,87 +7,76 @@ export const ErrorDiv = styled.div`
   align-items: center;
   left: 50%;
   top: 50%;
+
+  div {
+    position: absolute;
+    font-family: "Pacifico", cursive;
+    width: 700px;
+    font-weight: 500;
+    font-size: 100px;
+    color: #222222;
+    text-shadow: -6px 0 1px cyan, 6px 0 1px red;
+  }
 `;
 
 export const Bottom = styled.div`
-  position: absolute;
-  font-family: "Pacifico", cursive;
-  width: 700px;
-  font-weight: 500;
-  font-size: 100px;
-  color: #222222;
   animation: glitchBottom 1.4s linear infinite;
   clip-path: polygon(0 67%, 200% 67%, 200% 200%, 0 200%);
   -webkit-clip-path: polygon(0 67%, 200% 67%, 200% 200%, -100% 200%);
 
-  text-shadow: -7px 0 1px cyan, 7px 0 1px red;
-
   @keyframes glitchBottom {
     2%,
-    64% {
-      transform: translate(-2px, 0);
+    46% {
+      transform: translate(-3px, 0);
     }
     4%,
-    60% {
-      transform: translate(-2px, 0);
+    42% {
+      transform: translate(-3px, 0);
     }
-    62% {
-      transform: translate(-22px, 5px);
+    44% {
+      transform: translate(-26px, 4px);
     }
   }
 `;
 
 export const Title = styled.div`
-  position: absolute;
-  font-family: "Pacifico", cursive;
-  width: 700px;
-  font-weight: 500;
-  font-size: 100px;
-  color: #222222;
-  text-shadow: -7px 0 1.3px cyan, 7px 0 1px red;
   animation: glitch 1s linear infinite;
   clip-path: polygon(0 33%, 200% 0, 200% 67%, 0 67%);
   -webkit-clip-path: polygon(0 33%, 200% 0, 200% 67%, 0 67%);
 
   @keyframes glitch {
     2%,
-    64% {
+    46% {
       transform: translate(2px, 0);
     }
     4%,
-    60% {
+    42% {
       transform: translate(-2px, 0);
     }
-    62% {
+    44% {
       transform: translate(0, 0);
     }
   }
 `;
 
 export const Top = styled.div`
-  position: absolute;
-  font-family: "Pacifico", cursive;
-  width: 700px;
-  font-weight: 500;
-  font-size: 100px;
-  color: #222222;
-  text-shadow: -7px 0 1.2px cyan, 7px 0 1px red;
   animation: glitchTop 1s linear infinite;
   clip-path: polygon(0 0, 200% 0, 200% 33%, 0 33%);
   -webkit-clip-path: polygon(0 0, 200% 0, 200% 33%, 0 33%);
 
   @keyframes glitchTop {
     0%,
-    13% {
-      transform: translate(-5px, -2px);
+    48% {
+      transform: translate(-3px, 0);
     }
-    3% {
-      transform: translate(5px, -2px);
+    6%,
+    42% {
+      transform: translate(3px, 0);
     }
-    10% {
-      transform: translate(5px, -2px);
+    8% {
+      transform: translate(-5px, 0);
     }
-    11% {
+    44% {
       transform: translate(22px, -7px);
     }
   }
