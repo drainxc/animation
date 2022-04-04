ff76687bff159ce1b7ded215a22145fff01f3042
eastcopper <ldh7228@gmail.com>
2022-04-04 19:58:11 +0900

    ⚡ :: (#56)[Error] Refactoring

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -5,32 +5,16 @@ import { color } from "../../lib/export/data";
 
 export default function Error() {
   const title: string = "Lee DongHyeon";
-  const [random, setRandom] = useState<any>({
-    color1: color[0],
-    color2: color[0],
-    color3: color[0],
-    color4: color[0],
-    color5: color[0],
-    color6: color[0],
-    color7: color[0],
-    color8: color[0],
-    color9: color[0],
-  });
+  const [random, setRandom] = useState<any>(color);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const id = setInterval(() => {
-      setRandom({
-        color1: getRandomIntInclusive(0, 255),
-        color2: getRandomIntInclusive(0, 255),
-        color3: getRandomIntInclusive(0, 255),
-        color4: getRandomIntInclusive(0, 255),
-        color5: getRandomIntInclusive(0, 255),
-        color6: getRandomIntInclusive(0, 255),
-        color7: getRandomIntInclusive(0, 255),
-        color8: getRandomIntInclusive(0, 255),
-        color9: getRandomIntInclusive(0, 255),
-      });
+      setRandom(
+        random.map(() => ({
+          color: getRandomIntInclusive(0, 255),
+        }))
+      );
     }, 300);
     return () => clearInterval(id);
   }, [random]);
@@ -48,27 +32,27 @@ export default function Error() {
       <S.ErrorDiv>
         <S.Top>
           <S.Contents
-            color1={random.color1}
-            color2={random.color2}
-            color3={random.color3}
+            color1={random[0].color}
+            color2={random[1].color}
+            color3={random[2].color}
           >
             {title}
           </S.Contents>
         </S.Top>
         <S.Title>
           <S.Contents
-            color1={random.color4}
-            color2={random.color5}
-            color3={random.color6}
+            color1={random[3].color}
+            color2={random[4].color}
+            color3={random[5].color}
           >
             {title}
           </S.Contents>
         </S.Title>
         <S.Bottom>
           <S.Contents
-            color1={random.color7}
-            color2={random.color8}
-            color3={random.color9}
+            color1={random[6].color}
+            color2={random[7].color}
+            color3={random[8].color}
           >
             {title}
           </S.Contents>
