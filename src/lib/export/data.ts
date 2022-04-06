48f608269ece8a903a5ccdb4b1cbee1f60d325f1
eastcopper <ldh7228@gmail.com>
2022-04-07 08:52:23 +0900

    ⚡ :: (#83)[Data] Add Color

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -14,7 +14,14 @@ export const Title: string[] = [
 ];
 
 export const Color: string[][] = [
-  ["#ffffff", "#6dd5fa", "#2980b9"],
-  ["#ffffff", "#f5af19", "#f12711"],
-  ["#ffffff", "#38ef7d", "#11998e"],
+  ["tomato", "cyan", "yellow"],
+  ["#C6FFDD", "#FBD786", "#f7797d"],
+  ["#12c2e9", "#c471ed", "#f7797d"],
+  ["#8A2387", "#E94057", "#F27121"],
+  ["#1E9600", "#FFF200", "#FF0000"],
+  ["#40E0D0", "#FF8C00", "#FF0080"],
+  ["#ec38bc", "#7303c0", "#03001e"],
+  ["#fdbb2d", "#b21f1f", "#1a2a6c"],
+  ["#FDB99B", "#CF8BF3", "#A770EF"],
+  ["feac5e", "#c779d0", "#4BC0C8"],
 ];
