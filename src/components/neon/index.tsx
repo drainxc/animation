45076c969e054c9bf9a7600ad2f017811bef34de
eastcopper <ldh7228@gmail.com>
2022-04-06 15:58:23 +0900

    📄 :: (#76)[Neon] Add Contents

--- src/components/neon/index.tsx
+++ src/components/neon/index.tsx
@@ -18,8 +18,10 @@ export default function Neon() {
   return (
     <>
       <S.MainDiv>
-        {Title.map((res: string, i: number) => (
-          <S.Title style={{ animationDelay: `${i * 0.05}s` }}>{res}</S.Title>
+        {Title.map((word: string, i: number) => (
+          <S.Title style={{ animationDelay: `${Title.length * 0.2 + i * 0.05}s` }}>
+            <S.Contents style={{ animationDelay: `${i * 0.2}s` }}>{word}</S.Contents>
+          </S.Title>
         ))}
       </S.MainDiv>
     </>
