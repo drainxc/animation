33c2118daf799e15ad6d8a9782adc9e6bfbcd6ba
eastcopper <ldh7228@gmail.com>
2022-04-06 16:02:32 +0900

    ⚡ :: (#78)[Neon] Update Title

--- src/components/neon/index.tsx
+++ src/components/neon/index.tsx
@@ -1,26 +1,17 @@
 import * as S from "./styles";
+import { Title } from "../../lib/export/data";
 
 export default function Neon() {
-  const Title: any = [
-    "L",
-    "e",
-    "e",
-    "D",
-    "o",
-    "n",
-    "g",
-    "H",
-    "y",
-    "e",
-    "o",
-    "n",
-  ];
   return (
     <>
       <S.MainDiv>
         {Title.map((word: string, i: number) => (
-          <S.Title style={{ animationDelay: `${Title.length * 0.2 + i * 0.05}s` }}>
-            <S.Contents style={{ animationDelay: `${i * 0.2}s` }}>{word}</S.Contents>
+          <S.Title
+            style={{ animationDelay: `${Title.length * 0.2 + i * 0.1}s` }}
+          >
+            <S.Contents style={{ animationDelay: `${i * 0.2}s` }}>
+              {word}
+            </S.Contents>
           </S.Title>
         ))}
       </S.MainDiv>
