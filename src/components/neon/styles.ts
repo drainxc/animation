9e0ac366a4538c2032265a8d9bc3f37ccc1b1a83
eastcopper <ldh7228@gmail.com>
2022-04-06 15:24:23 +0900

    📄 :: (#72)[Neon] Add Glow

--- src/components/neon/styles.ts
+++ src/components/neon/styles.ts
@@ -7,4 +7,19 @@ export const MainDiv = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+  background-color: #222222;
+`;
+
+export const Title = styled.div`
+  color: #f2f2f2;
+  font-size: 100px;
+  font-family: "Pacifico", cursive;
+  font-weight: 900;
+  animation: glow 1s ease-in-out both infinite;
+
+  @keyframes glow {
+    50% {
+      text-shadow: 0 -15px 25px cyan, -15px 15px 25px yellowgreen;
+    }
+  }
 `;
