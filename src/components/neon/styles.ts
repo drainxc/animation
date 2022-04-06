ac05ca83d1bd14214579581b7c96788c2fa07090
eastcopper <ldh7228@gmail.com>
2022-04-06 14:54:00 +0900

    ⚡ :: (#71)[Neon] Design Neon

---
+++ src/components/neon/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
