fef7cb4137a546ecc64a0df04a998836c8eb37b2
eastcopper <ldh7228@gmail.com>
2022-04-07 12:12:32 +0900

    ⚡ :: (#88)[Routes] Update Routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Bounce from "../components/bounce";
+import Error from "../components/error";
+import Neon from "../components/neon";
 import Zipper from "../components/zipper";
 
 export default function Router() {
   return (
     <>
-      <Zipper />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/bounce" element={<Bounce />} />
+          <Route path="/neon" element={<Neon />} />
+          <Route path="/zipper" element={<Zipper />} />
+          <Route path="/error" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
